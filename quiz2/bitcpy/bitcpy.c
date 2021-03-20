1fa2cfbcd6b43ef9226903dae2d2a191b0412304
Nahemah Chen <a122774007@gmail.com>
2021-03-20 10:02:07 +0800

    improve cpu miss rate and replace mask by bitwise

--- quiz2/bitcpy/bitcpy.c
+++ quiz2/bitcpy/bitcpy.c
@@ -15,52 +15,23 @@ void bitcpy(void *_dest,      /* Address of the buffer to write to */
     size_t write_rhs = 8 - write_lhs;
     uint8_t *dest = (uint8_t *) _dest + (_write / 8);
 
-    static const uint8_t read_mask[] = {
-        0x00, /*    == 0    00000000b   */
-        0x80, /*    == 1    10000000b   */
-        0xC0, /*    == 2    11000000b   */
-        0xE0, /*    == 3    11100000b   */
-        0xF0, /*    == 4    11110000b   */
-        0xF8, /*    == 5    11111000b   */
-        0xFC, /*    == 6    11111100b   */
-        0xFE, /*    == 7    11111110b   */
-        0xFF  /*    == 8    11111111b   */
-    };
-
-    static const uint8_t write_mask[] = {
-        0xFF, /*    == 0    11111111b   */
-        0x7F, /*    == 1    01111111b   */
-        0x3F, /*    == 2    00111111b   */
-        0x1F, /*    == 3    00011111b   */
-        0x0F, /*    == 4    00001111b   */
-        0x07, /*    == 5    00000111b   */
-        0x03, /*    == 6    00000011b   */
-        0x01, /*    == 7    00000001b   */
-        0x00  /*    == 8    00000000b   */
-    };
-
     while (count > 0) {
         uint8_t data = *source++;
         size_t bitsize = (count > 8) ? 8 : count;
-        if (read_lhs > 0) {
-            data <<= read_lhs;
-            if (bitsize > read_rhs)
-                data |= (*source >> read_rhs);
-        }
-
-        if (bitsize < 8)
-            data &= read_mask[bitsize];
+        data <<= read_lhs;
+        data |= (*source >> read_rhs);
+        data &= 0xFF << (8 - bitsize);
 
         uint8_t original = *dest;
-        uint8_t mask = read_mask[write_lhs];
+        uint8_t mask = 0xFF << (8 - write_lhs);
         if (bitsize > write_rhs) {
             /* Cross multiple bytes */
             *dest++ = (original & mask) | (data >> write_lhs);
-            original = *dest & write_mask[bitsize - write_rhs];
+            original = *dest & (0xFF >> (bitsize - write_rhs));
             *dest = original | (data << write_rhs);
         } else {
             // Since write_lhs + bitsize is never >= 8, no out-of-bound access.
-            DDD;
+            mask |= (0xFF >> (bitsize - write_lhs));
             *dest++ = (original & mask) | (data >> write_lhs);
         }
 
